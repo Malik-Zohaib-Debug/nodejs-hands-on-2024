19f58f1bd9c528f8908abfbd1d12efc54ed011f7
Malik Zohaib Zahid <64071105+Malik-Zohaib-Debug@users.noreply.github.com>
2024-05-12 17:53:17 +0500

    created logger middleware

--- server2.js
+++ server2.js
@@ -7,24 +7,32 @@ const users = [
   { id: 3, name: "Jim Doe" },
 ];
 
+// defined a middleware
+const logger = ((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
 const server = createServer((req, res) => {
-  if (req.url === "/api/user" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify(users));
-    res.end();
-  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-    const id = req.url.split('/')[3];
-    const user = users.find((user) => user.id === parseInt(id));
-    if (user) {
-      res.setHeader("Content-Type", "application/json");
-      res.write(JSON.stringify(user));
-      res.end();
-    }
-  } else {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({ message: "Route not found" }));
-    res.end();
-  }
+  logger(req, res, () => {
+    if (req.url === "/api/user" && req.method === "GET") {
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(users));
+        res.end();
+      } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        if (user) {
+          res.setHeader("Content-Type", "application/json");
+          res.write(JSON.stringify(user));
+          res.end();
+        }
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify({ message: "Route not found" }));
+        res.end();
+      }
+  })
 });
 
 server.listen(PORT, () => {
