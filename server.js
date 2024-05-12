ed37c7f165361ae81dff3d060332e91317209989
Malik Zohaib Zahid <64071105+Malik-Zohaib-Debug@users.noreply.github.com>
2024-05-12 15:58:58 +0500

    rendered html pages with node fs

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
     // res.setHeader('Content-Type', 'text/plain');
     // res.statusCode = 403;
     // res.write("<h1>Hello,World!</h1>");
     
-    //writeHead 
-    res.writeHead(500, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: "Server Error"}))
+    try {
+        if(req.method === 'GET'){
+            let filePath;
+            if(req.url === '/'){
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if(req.url === '/about'){
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found');
+            }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('Method Not Found');
+        }
+    } catch(error){
+
+        //writeHead 
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: "Server Error"}))
+    }
+
 })
 
 server.listen(PORT, () => {
